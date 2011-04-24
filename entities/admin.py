eda6b26756373e62f707cf39608b9ee0cc0729ec
Kirill Shileev <kshileev@gmail.com>
2011-04-24 18:23:52 +0400

    register model classes to admin interface

---
+++ entities/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+import models
+
+admin.site.register(models.LegalEntity)
+admin.site.register(models.Person)
+
+
+  
---
+++ finances/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+import models
+
+admin.site.register(models.Account)
+admin.site.register(models.Bank)
+admin.site.register(models.Transaction)
