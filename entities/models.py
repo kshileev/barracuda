c12133badceac5adfb77b7b37f7c35f0ecd9ccb0
Kirill Shileev <kshileev@gmail.com>
2011-04-24 22:49:36 +0400

    add Meta to entities classes

--- entities/models.py
+++ entities/models.py
@@ -17,6 +17,10 @@ PERSON_TYPE = (
 )
 
 class LegalEntity(models.Model):
+    class Meta:
+        verbose_name = 'Юрлицо'
+        verbose_name_plural = 'Юрлица'
+        
     title		= models.CharField(verbose_name = 'название', max_length = 255)
     address		= models.TextField(verbose_name = 'адрес', max_length = 512)
     type		= models.CharField(verbose_name = 'тип', max_length = 4, choices = LEGAL_ENTITY_TYPE)
@@ -25,6 +29,10 @@ class LegalEntity(models.Model):
         return self.title
 
 class Person(models.Model):
+    class Meta:
+        verbose_name = 'Человек'
+        verbose_name_plural = 'Люди'
+
     name = models.CharField(max_length = 64, unique = True, verbose_name = 'имя')
     type = models.CharField(verbose_name='тип', max_length=4, choices= PERSON_TYPE)
 
