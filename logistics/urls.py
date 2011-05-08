a012e5200c3e3ba0b7340b4e0e9dc8bd3a62a9d6
Kirill Shileev <kshileev@griddynamics.com>
2011-05-09 02:30:58 +0400

    add logistics/customs and fix non absolute paths in menu.html

--- logistics/urls.py
+++ logistics/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls.defaults import patterns, url
-from django.views.generic import ListView
-from models import Conveyance, Warehouse
+from django.views.generic import ListView, list_detail
+from models import Conveyance, Warehouse, Customs
 
 urlpatterns = patterns('',
     url(r'^$', ListView.as_view(queryset=Conveyance.objects, template_name='logistics.html')),
-    url(r'warehouses/$', ListView.as_view(queryset=Warehouse.objects, template_name='warehouses.html')),
+    url(r'warehouses/$', list_detail.object_list, {'queryset':Warehouse.objects.all(), 'template_name':'warehouses.html'}),
+    url(r'customs/$', list_detail.object_list, {'queryset':Customs.objects.all(),
+                                                'template_name':'customs.html',
+                                                'template_object_name':'customs'
+                                                })
 )
   
