569eadebd38840ff6922a3fb7e9659aba45a8ceb
Kirill Shileev <kshileev@griddynamics.com>
2011-05-09 00:18:16 +0400

    add warehouses url and template

--- logistics/urls.py
+++ logistics/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls.defaults import patterns, url
 from django.views.generic import ListView
-from models import Conveyance
+from models import Conveyance, Warehouse
 
 urlpatterns = patterns('',
     url(r'^$', ListView.as_view(queryset=Conveyance.objects, template_name='logistics.html')),
+    url(r'warehouses/$', ListView.as_view(queryset=Warehouse.objects, template_name='warehouses.html')),
 )
   
