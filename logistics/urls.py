446c0f12611aff9428fbf9a443a1891dafc12dff
Kirill Shileev <kshileev@griddynamics.com>
2011-05-09 03:34:56 +0400

    add logistics/conveyances

--- logistics/urls.py
+++ logistics/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls.defaults import patterns, url
-from django.views.generic import ListView, list_detail
+from django.views.generic import list_detail
 from models import Conveyance, Warehouse, Customs
 
 urlpatterns = patterns('',
-    url(r'^$', ListView.as_view(queryset=Conveyance.objects, template_name='logistics.html')),
+    url(r'^conveyances/$', list_detail.object_list, {
+                                                'queryset':Conveyance.objects.all(),
+                                                'template_name':'conveyances.html',
+                                                'template_object_name':'conveyances_list'
+                                                }),
     url(r'warehouses/$', list_detail.object_list, {
                                                 'queryset':Warehouse.objects.all(),
                                                 'template_name':'warehouses.html',
