7fb329c89cc2c1ef5aba4aebbbf4b74e1102ef00
Kirill Shileev <kshileev@griddynamics.com>
2011-05-09 00:10:13 +0400

    add core application to the list, change DB location

--- settings.py
+++ settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'C:/Users/kshileev/Barracuda.db', # Or path to database file if using sqlite3.
+        'NAME': 'Barracuda.db', # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -116,7 +116,8 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.admindocs',
     'django_extensions',
-    
+
+    'barracuda.core',
     'barracuda.logistics',
     'barracuda.finances',
     'barracuda.entities'
