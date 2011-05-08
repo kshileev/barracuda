c753a51774268e726ba580c28a19d64726a3ebd5
Kirill Shileev <kshileev@griddynamics.com>
2011-05-09 02:33:45 +0400

    fix minor problems in logistics/warehouses

--- logistics/urls.py
+++ logistics/urls.py
@@ -4,7 +4,11 @@ from models import Conveyance, Warehouse, Customs
 
 urlpatterns = patterns('',
     url(r'^$', ListView.as_view(queryset=Conveyance.objects, template_name='logistics.html')),
-    url(r'warehouses/$', list_detail.object_list, {'queryset':Warehouse.objects.all(), 'template_name':'warehouses.html'}),
+    url(r'warehouses/$', list_detail.object_list, {
+                                                'queryset':Warehouse.objects.all(),
+                                                'template_name':'warehouses.html',
+                                                'template_object_name':'warehouses'
+                                                }),
     url(r'customs/$', list_detail.object_list, {'queryset':Customs.objects.all(),
                                                 'template_name':'customs.html',
                                                 'template_object_name':'customs'
