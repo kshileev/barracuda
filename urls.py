8ee77c9dccec5a4dcc1289e791a770bfdb59265f
Kirill Shileev <kshileev@griddynamics.com>
2011-05-09 23:09:56 +0400

    upgrade generic views from from function based to class based

--- urls.py
+++ urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
-from django.views.generic.simple import direct_to_template
-from django.views.generic import list_detail
+from django.views.generic import ListView, TemplateView
 from django.contrib import admin
 from django.contrib.auth.views import login,logout
 admin.autodiscover()
@@ -12,22 +11,11 @@ urlpatterns = patterns('',
     url(r'^$', login),
     url(r'^accounts/profile/$', 'core.views.after_login'),
     url(r'^logout/$', logout),
-    url(r'^about/$', direct_to_template, {'template':'about.html'})
+    url(r'^about/$', TemplateView.as_view(template_name='about.html'))
 )
 
 urlpatterns += patterns('',
-    url(r'^logistics/conveyances/$', list_detail.object_list, {
-                                                'queryset':Conveyance.objects.all(),
-                                                'template_name':'conveyances.html',
-                                                'template_object_name':'conveyances_list'
-                                                }),
-    url(r'^logistics/warehouses/$', list_detail.object_list, {
-                                                'queryset':Warehouse.objects.all(),
-                                                'template_name':'warehouses.html',
-                                                'template_object_name':'warehouses'
-                                                }),
-    url(r'^logistics/customs/$', list_detail.object_list, {'queryset':Customs.objects.all(),
-                                                'template_name':'customs.html',
-                                                'template_object_name':'customs'
-                                                })
+    url(r'^logistics/conveyances/$', ListView.as_view(model=Conveyance,context_object_name='conveyance_list')),
+    url(r'^logistics/warehouses/$', ListView.as_view(model=Warehouse,context_object_name='warehouse_list')),
+    url(r'^logistics/customs/$', ListView.as_view(model=Customs,context_object_name='customs_list'))
 )
