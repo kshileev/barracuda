33e18b9f2148e414c4b3d3ef0361d8eb2e09c49b
unknown <kir@.(none)>
2011-08-18 20:15:42 +0400

    fix bug to use STATIC_URL

--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
 # Create your views here.
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 def after_login(request):
-    return render_to_response('tmp.html', {'user':request.user})
+    return render_to_response('tmp.html', {'user':request.user},
+                              context_instance=RequestContext(request))
