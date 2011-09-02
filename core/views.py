485b7c1e4795e9e6df72d7ae9423d93b8b28ce11
Kirill Shileev <kshileev@gmail.com>
2011-09-03 02:50:45 +0400

    add feedback form

--- core/views.py
+++ core/views.py
@@ -1,7 +1,18 @@
 # Create your views here.
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.views.generic import FormView
+from core.forms import FeedbackForm
 
 def after_login(request):
-    return render_to_response('dashboard.html', {'user':request.user},
-                              context_instance=RequestContext(request))
+    return render_to_response('dashboard.html', {'user': request.user}, context_instance=RequestContext(request))
+
+class FeedbackView(FormView):
+    template_name = 'core/feedback.html'
+    success_url = '/feedback/'
+    form_class = FeedbackForm
+    
+    def form_valid(self, form):
+        print 'Eh'
+        return HttpResponseRedirect(self.success_url)
--- urls.py
+++ urls.py
@@ -1,22 +1,29 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.views.generic import ListView, TemplateView
 from django.contrib import admin
-from django.contrib.auth.views import login,logout
+from django.contrib.auth.views import login, logout
+from core.views import FeedbackView
+
 admin.autodiscover()
 from logistics.models import Conveyance, Warehouse, Customs
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = patterns('',
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', login_required(TemplateView.as_view(template_name='dashboard.html'))),
-    url(r'^accounts/login/$', login),
-    url(r'^logout/$', logout),
-    url(r'^about/$', TemplateView.as_view(template_name='about.html'))
+                       url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^$', login_required(TemplateView.as_view(template_name='dashboard.html'))),
+                       url(r'^accounts/login/$', login),
+                       url(r'^logout/$', logout),
+                       url(r'^about/$', TemplateView.as_view(template_name='about.html')),
+                       url(r'^feedback/$', FeedbackView.as_view(), name='feedback')
+
 )
 
 urlpatterns += patterns('',
-    url(r'^logistics/conveyances/$', login_required(ListView.as_view(model=Conveyance,context_object_name='conveyance_list'))),
-    url(r'^logistics/warehouses/$', login_required(ListView.as_view(model=Warehouse,context_object_name='warehouse_list'))),
-    url(r'^logistics/customs/$', login_required(ListView.as_view(model=Customs,context_object_name='customs_list')))
+                        url(r'^logistics/conveyances/$',
+                            login_required(ListView.as_view(model=Conveyance, context_object_name='conveyance_list'))),
+                        url(r'^logistics/warehouses/$',
+                            login_required(ListView.as_view(model=Warehouse, context_object_name='warehouse_list'))),
+                        url(r'^logistics/customs/$',
+                            login_required(ListView.as_view(model=Customs, context_object_name='customs_list')))
 )
